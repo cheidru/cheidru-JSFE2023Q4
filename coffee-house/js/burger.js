7a52d4d7aaaf37da0a63bb557a6853f38a29ca44
Чечеуров Игорь Дмитриевич <ichecheurov@stan-group.com>
2023-12-11 16:21:15 +0300

    fix: fix burger menu for menu page

--- coffee-house/js/burger.js
+++ coffee-house/js/burger.js
@@ -1,19 +1,21 @@
 const burgerHomeBTN = document.getElementById('header-menu');
 const burgerHomeMenu = document.getElementById('burger-mnu');
 const burgerMenuBTN = document.getElementById('header-menu-menu');
-const burgerMenuMenuBTN = document.getElementById('burger-mnu-btn');
+// const burgerMenuMenuBTN = document.getElementById('burger-mnu-btn');
 const burgerBTNbarOne = document.getElementById('burger-btn-bar-one');
 const burgerBTNbarTwo = document.getElementById('burger-btn-bar-two');
 
 
 let burgerMenuON = false;
 
-burgerHomeBTN.addEventListener('click', () => {
-    let burgerMNUStyle = getComputedStyle(burgerHomeMenu);
-    if(burgerMNUStyle.display == 'flex') {
-        burgerMenuON == false ? showBurgerMenu() : hideBurgerMenu();
-    }
-})
+if(burgerHomeBTN) {
+    burgerHomeBTN.addEventListener('click', () => {
+        let burgerMNUStyle = getComputedStyle(burgerHomeMenu);
+        if(burgerMNUStyle.display == 'flex') {
+            burgerMenuON == false ? showBurgerMenu() : hideBurgerMenu();
+        }
+    })
+}
 
 burgerHomeMenu.addEventListener('click', (event) => {
     console.log(event.target.parentNode.tagName);
@@ -21,6 +23,20 @@ burgerHomeMenu.addEventListener('click', (event) => {
         hideBurgerMenu();        
     }
 })
+
+burgerMenuBTN.addEventListener('click', () => {
+    let burgerMNUStyle = getComputedStyle(burgerHomeMenu);
+    if(burgerMNUStyle.display == 'flex') {
+        burgerMenuON == false ? showBurgerMenuMenu() : hideBurgerMenuMenu();
+    }
+})
+
+// burgerMenuMenuBTN.addEventListener('click', (event) => {
+//     console.log(event.target.parentNode.tagName);
+//     if (event.target.parentNode.tagName == 'LI' || event.target.parentNode == burgerMenuMenuBTN) {
+//         hideBurgerMenu();        
+//     }
+// })
 
 function showBurgerMenu() {
     burgerHomeMenu.classList.add('engaged');
@@ -31,6 +47,19 @@ function showBurgerMenu() {
 function hideBurgerMenu() {
     burgerHomeMenu.classList.remove('engaged');
     burgerHomeBTN.classList.remove('engaged');
+    burgerMenuON = false;
+}
+
+
+function showBurgerMenuMenu() {
+    burgerHomeMenu.classList.add('engaged');
+    burgerMenuBTN.classList.add('engaged');
+    burgerMenuON = true;
+}
+
+function hideBurgerMenuMenu() {
+    burgerHomeMenu.classList.remove('engaged');
+    burgerMenuBTN.classList.remove('engaged');
     burgerMenuON = false;
 }
 
