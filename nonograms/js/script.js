12b96a97ab25e3a6ea2037ca2baac4a739019ddf
Чечеуров Игорь Дмитриевич <ichecheurov@stan-group.com>
2024-01-29 17:17:14 +0300

    feat: add nonogram drawing functionality

--- nonograms/js/script.js
+++ nonograms/js/script.js
@@ -1,20 +1,33 @@
 const field = document.getElementById('play-ground-wrapper');
 
+const NEW_GAME = false;
+user = {};
+user.lastGame = {};
+user.lastGame.level = 0;
+user.lastGame.number = 0;
+user.lastGame.result = [];
+user.default = {};
+user.default.level = 0;
+user.default.theme = 'light-theme';
+user.bestResults = {};
+
 const game = {}
 game.difficulty = 0;
-game.template = {};
+game.number = 0;
+game.name = '';
+game.template = [];
+game.guesses = [];
 
-function gameStart() {
-  readTemplate();
+function startGame() {
+  selectRandomGame(NEW_GAME);
+  drawNonogram();
 
   field.addEventListener('contextmenu', (e) => {
     e.preventDefault();
 
     if(e.target.classList.contains('cell')) {
-      console.log('right-click');
       e.target.classList.remove('guess-item');
       e.target.classList.toggle('cross-item');
-      console.log('right-click', e.target.classList);
     }
   });
 
@@ -22,14 +35,50 @@ function gameStart() {
     if(e.target.classList.contains('cell')) {
       e.target.classList.remove('cross-item');
       e.target.classList.toggle('guess-item');
-      console.log('left-click', e.target.classList);
     }
 
   })
 }
 
-function readTemplate() {
-  if (game.difficulty === 0) game.template = easyTemplate[0];
+function drawNonogram() {
+  switch(game.level) {
+    case 0:
+      game.template = easyTemplate[game.number].pictire;
+      game.name = easyTemplate[game.number].name;
+      break;
+    case 1:
+      game.template = mediumTemplate[game.number].pictire;
+      game.name = easyTemplate[game.number].name;
+      break;
+    case 2:
+      game.template = hardTemplate[game.number].pictire;
+      game.name = easyTemplate[game.number].name;
+      break;
+  }
+
+  const nonoField = document.getElementById('play-ground-wrapper');
+  console.log('start drawing');
+  let nonoItemID = 1;
+  for (let i = 0; i < game.template.pictire.length + 1; i++) {
+    for (let j = 0; j < game.template.pictire.length + 1; j++) {   
+      const nonoItem = document.createElement('div');
+      nonoItem.classList.add('item');
+      if (i === 0) {
+        if (j === 0) nonoItem.classList.add('empty');
+        nonoItem.classList.add('clue-top');
+      } else {
+        if (j === 0) {
+          nonoItem.classList.add('clue-left');
+        } else {
+          nonoItem.classList.add('cell');
+          nonoItem.setAttribute('id',`${nonoItemID}`);
+          nonoItemID++;
+        }
+      }
+      nonoField.append(nonoItem);
+    }
+  }
+
   const topClues = document.querySelectorAll('.clue-top');
   const leftClues = document.querySelectorAll('.clue-left');
 
@@ -39,4 +88,13 @@ function readTemplate() {
   }
 }
 
-gameStart();
+function selectRandomGame(randomButtonPressed) {
+  if(randomButtonPressed) game.level = randomChoice(3);
+  game.number = randomChoice(5);
+
+  function randomChoice(range) {
+    return Math.round(Math.random() * range);
+  }
+}
+
+startGame();
