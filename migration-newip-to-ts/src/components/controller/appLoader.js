14fadc87424c0fd163ab47f7b3baf0243e0cd07c
Igor Checheurov <chei@yandex.ru>
2024-02-27 23:45:15 +0300

    feat: load headers drom API

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -1,12 +1,12 @@
 import Loader from './loader';
 
 class AppLoader extends Loader {
-    static API_KEY = 'a2db0eb7b478479fb9dce3efcd99e978';
     constructor() {
-        super(process.env.API_URL, {
-            // apiKey: 'a2db0eb7b478479fb9dce3efcd99e978',
-            apiKey: process.env.API_KEY,
-        });
+        super(
+            'https://newsapi.org/v2/top-headlines/',
+
+            { apiKey: 'a2db0eb7b478479fb9dce3efcd99e978', }
+        );
     }
 }
 
