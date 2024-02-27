7b702e5189171d823c071b226af93bd837fc3d0a
Igor Checheurov <chei@yandex.ru>
2024-02-27 23:07:33 +0300

    fix: restore missing html file

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -1,10 +1,11 @@
 import Loader from './loader';
 
 class AppLoader extends Loader {
+    static API_KEY = 'a2db0eb7b478479fb9dce3efcd99e978';
     constructor() {
         super(process.env.API_URL, {
-            apiKey: a2db0eb7b478479fb9dce3efcd99e978,
-            // apiKey: process.env.API_KEY,
+            // apiKey: 'a2db0eb7b478479fb9dce3efcd99e978',
+            apiKey: process.env.API_KEY,
         });
     }
 }
