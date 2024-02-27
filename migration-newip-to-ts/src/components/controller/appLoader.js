256f2d524b9fcd4525f21582de6eefb6dc9cf040
Igor Checheurov <chei@yandex.ru>
2024-02-27 20:53:30 +0300

    feat: setup configuration files

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,8 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super(process.env.API_URL, {
-            apiKey: process.env.API_KEY,
+            apiKey: a2db0eb7b478479fb9dce3efcd99e978,
+            // apiKey: process.env.API_KEY,
         });
     }
 }
