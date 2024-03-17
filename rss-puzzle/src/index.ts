4f1c91241b55725eccbd1ee9ca3e5276886cbfe0
cheidru <74682438+cheidru@users.noreply.github.com>
2024-03-18 07:42:37 +1100

    feat: add greeting window (#102)

--- rss-puzzle/src/index.ts
+++ rss-puzzle/src/index.ts
@@ -1,6 +1,10 @@
 import { startLogin } from './login';
 
-// Create header with logout button
+const userData = {
+    firstName: '',
+    lastName: '',
+};
+
 const header = document.createElement('header');
 const logoutBTN = document.createElement('button');
 logoutBTN.textContent = 'Log Out';
@@ -66,10 +70,21 @@ const startGameBTN = document.createElement('button');
 startGameBTN.innerText = 'Start Plying';
 startScreen.append(startGameBTN);
 
-// let userData = {
-//     firstName: '',
-//     lastName: '',
-// };
+const greetingWindow = document.createElement('div');
+greetingWindow.setAttribute('id', 'greeting-wrapper');
+greetingWindow.classList.add('modal');
+greetingWindow.classList.add('hidden-modal');
+document.body.append(greetingWindow);
+
+const greetingTxt = document.createElement('p');
+greetingTxt.setAttribute('id', 'greeting-txt');
+greetingWindow.append(greetingTxt);
+
+const greetingClose = document.createElement('div');
+greetingClose.setAttribute('id', 'greeting-close');
+greetingClose.innerHTML =
+    '<svg xmlns="http://www.w3.org/2000/svg" class="greetng-close-svg" viewBox="0 0 24 24"><path d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2C6.47,2 2,6.47 2,12C2,17.53 6.47,22 12,22C17.53,22 22,17.53 22,12C22,6.47 17.53,2 12,2M14.59,8L12,10.59L9.41,8L8,9.41L10.59,12L8,14.59L9.41,16L12,13.41L14.59,16L16,14.59L13.41,12L16,9.41L14.59,8Z" /></svg>';
+greetingWindow.append(greetingClose);
 
 prepareUser();
 
@@ -84,47 +99,68 @@ function prepareUser() {
         document.body.append(header);
         header.append(logoutBTN);
         logoutBTN.addEventListener('click', () => {
-            console.log('Erase user');
-            logOutWindow.classList.remove('hidden-modal');
-
-            logOutCancelBTN.addEventListener(
-                'click',
-                () => {
-                    console.log('cancel clicked');
-                    logOutWindow.classList.add('hidden-modal');
-                },
-                { once: true }
-            );
-
-            logOutConfirmBTN.addEventListener(
-                'click',
-                () => {
-                    header.style.display = 'none';
-                    logOutWindow.classList.add('hidden-modal');
-                    localStorage.removeItem('rss-puzzle-user');
-                    prepareUser();
-                },
-                { once: true }
-            );
+            logUserOut();
         });
         showStartScreen();
     });
 }
 
+function logUserOut() {
+    logOutWindow.classList.remove('hidden-modal');
+
+    logOutCancelBTN.addEventListener(
+        'click',
+        () => {
+            logOutWindow.classList.add('hidden-modal');
+        },
+        true
+    );
+
+    logOutConfirmBTN.addEventListener(
+        'click',
+        () => {
+            header.style.display = 'none';
+            logOutWindow.classList.add('hidden-modal');
+            localStorage.removeItem('rss-puzzle-user');
+            prepareUser();
+        },
+        true
+    );
+}
+
 function checkLocalStorage() {
     const storageData = localStorage.getItem('rss-puzzle-user');
     if (storageData === null) return false;
-    // userData = { ...JSON.parse(storageData) };
+    const localData = { ...JSON.parse(storageData) };
+    userData.firstName = localData.firstName;
+    userData.lastName = localData.lastName;
     return true;
 }
 
 function showStartScreen() {
-    header.style.display = 'flex';
+    header.style.display = 'none';
     startScreen.classList.remove('hidden-modal');
-    startGameBTN.addEventListener('click', () => {
-    }, true)
+    console.log('firstName & lastName = ', userData.firstName, userData.lastName);
+    startGameBTN.addEventListener(
+        'click',
+        () => {
+            startScreen.classList.add('hidden-modal');
+            startGame();
+        },
+        true
+    );
+}
 
-    // const header = document.createElement('header');
-    // document.body.append(header);
-    // header.append(logoutBTN);
+function startGame() {
+    greetingTxt.textContent = `Dear ${userData.firstName} ${userData.lastName} ! 
+    We appreciate Your interest in RSS Puzzle game. 
+    Hope You enjoy playing and learn a lot of English vocabulary.`;
+    greetingWindow.classList.remove('hidden-modal');
+    greetingClose.addEventListener(
+        'click',
+        () => {
+            greetingWindow.classList.add('hidden-modal');
+        },
+        true
+    );
 }
